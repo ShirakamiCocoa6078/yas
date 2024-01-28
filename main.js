e71125fbbd9251734031873e1162019d50056ef4
Shirakami_Cocoa6078 <71052778+ShirakamiCocoa6078@users.noreply.github.com>
2024-01-29 05:53:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function create(){
-  document.getElementById("ver").innerText="ver:1.2";
+  document.getElementById("ver").innerText="ver:1.2.1";
   let value = ""
   let emojiStr = ""
   let wordStr = ""

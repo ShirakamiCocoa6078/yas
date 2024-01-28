7ecb0948037fa2aa88239b3ad9a2acb74bd96ad3
Shirakami_Cocoa6078 <71052778+ShirakamiCocoa6078@users.noreply.github.com>
2024-01-29 06:26:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
-let emoji = "❤,💖,💕,❤️‍🔥,❤️‍🩹,💓".split(",")
-let word1 = "아,앗,앙,오,옷,우,웅,웃,으,읏,응,잇,엣".split(",")
-let word2 ="흐,하,헤,핫,호,흣".split(",")
+let emoji = "❤,💖,💕,❤️‍🔥,❤️‍🩹,💓".split(",");
+let word1 = "아,앗,앙,오,옷,우,웅,웃,으,읏,응,잇,엣".split(",");
+let word2 = "흐,하,헤,핫,호,흣".split(",");
+
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function create(){
-  document.getElementById("ver").innerText="script ver:1.2.7";
-  let value = ""
-  let emojiStr = ""
-  let wordStr = ""
-  for(let i=0;i<Number(document.getElementById("sentenceLength").value;i++)){
+
+function create() {
+  document.getElementById("ver").innerText = "script ver:1.2.8";
+  let value = "";
+  let emojiStr = "";
+  let wordStr = "";
+
+  for (let i = 0; i < Number(document.getElementById("sentenceLength").value); i++) {
+    let emojiCount = rand(0, Number(document.getElementById("heartCount").value));
     
-    let emojiCount = rand(0,Number(document.getElementById("heartCount").value));
-    if(emojicount!=0){
-      for(let j=0;j<emojiCount;j++){
-        let emojiNum = rand(0,5)
-        emojiStr += emoji[emojiNum]
+    if (emojiCount !== 0) {
+      for (let j = 0; j < emojiCount; j++) {
+        let emojiNum = rand(0, 5);
+        emojiStr += emoji[emojiNum];
       }
     }
-    
-    let wordCount = rand(1,Number(document.getElementById("charLimit").value));
-    let word2Num = rand(0,5)
-    wordStr += word2[word2Num]
-    for(let j=0;j<wordCount-1;j++){
-      let word1Num = rand(0,12)
-      wordStr += word1[word1Num]
+
+    let wordCount = rand(1, Number(document.getElementById("charLimit").value));
+    let word2Num = rand(0, 5);
+    wordStr += word2[word2Num];
+
+    for (let j = 0; j < wordCount - 1; j++) {
+      let word1Num = rand(0, 12);
+      wordStr += word1[word1Num];
     }
-    
-    let pointCount = rand(2,5)
-    let randomPick = wordStr+emojiStr+".".repeat(pointCount)
-    value += randomPick
-    emojiStr= ""
-    wordStr = ""
+
+    let pointCount = rand(2, 5);
+    let randomPick = wordStr + emojiStr + ".".repeat(pointCount);
+    value += randomPick;
+
+    emojiStr = "";
+    wordStr = "";
   }
-  let element=document.getElementById("main");
-element.innerText = value
+
+  let element = document.getElementById("main");
+  element.innerText = value;
 }
